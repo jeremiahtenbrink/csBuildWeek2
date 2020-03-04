204d218dea9e37b5e9138699750cdf0aa42ff363
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-03-04 11:04:55 -0700

    fixed move bug.

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ module.exports = (req, res, next) => {
   
   if (uid === "bcff423ac6f6c8b0994654ccf917fb0c1e4699ca"){
     next();
+  }else {
+    res.status(500).json({message: "You are not allowed to make requests."})
   }
-  res.status(500).json({message: "You are not allowed to make requests."})
 };
