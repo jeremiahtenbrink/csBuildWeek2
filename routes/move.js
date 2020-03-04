f625890e78511a970974ba9e0e3aceec86c08d7e
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-03-04 00:42:24 -0700

    added json parse for items.

--- routes/move.js
+++ routes/move.js
@@ -112,6 +112,7 @@ router.get('/title', async (req, res) => {
   Object.keys(map).forEach(key => {
     mapTitle[map[key].title + " " + map[key].room_id] = map[key];
   });
+  mapTitle["# rooms"] = map.length;
   res.status(200).json(mapTitle);
 });
 
